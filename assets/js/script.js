639c1e2430a03c3a0e38f22bb6c6115d8f86fce7
somrat <monnaf37@gmail.com>
2020-04-15 11:36:36 +0600

    modified preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,36 @@
-(function ($) {
-  'use strict';
-
   // Preloader js    
   $(window).on('load', function () {
     $('.preloader').fadeOut(100);
   });
 
+  (function ($) {
+    'use strict';
 
-  //  Search Form Open
-  $('#searchOpen').on('click', function () {
-    $('.search-wrapper').addClass('open');
-  });
-  $('#searchClose').on('click', function () {
-    $('.search-wrapper').removeClass('open');
-  });
+    //  Search Form Open
+    $('#searchOpen').on('click', function () {
+      $('.search-wrapper').addClass('open');
+    });
+    $('#searchClose').on('click', function () {
+      $('.search-wrapper').removeClass('open');
+    });
 
-  // featured post slider
-  $('.featured-post-slider').slick({
-    infinite: true,
-    vertical: true,
-    verticalSwiping: true,
-    arrows: false,
-    dots: true,
-    responsive: [{
-      breakpoint: 600,
-      settings: {
-        vertical: false,
-        verticalSwiping: false,
-      }
-    }]
-  });
+    // featured post slider
+    $('.featured-post-slider').slick({
+      infinite: true,
+      vertical: true,
+      verticalSwiping: true,
+      arrows: false,
+      dots: true,
+      responsive: [{
+        breakpoint: 600,
+        settings: {
+          vertical: false,
+          verticalSwiping: false,
+        }
+      }]
+    });
 
-  // venobox initialize
-  $('.venobox').venobox();
+    // venobox initialize
+    $('.venobox').venobox();
 
-})(jQuery);
+  })(jQuery);
